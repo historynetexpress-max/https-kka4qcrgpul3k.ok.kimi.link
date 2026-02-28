46c1e18ee9e2c94139716d4e25e9c2a71f670b20
PQP CORE <historynetexpress@gmail.com>
2026-02-28 07:33:07 +0530

    Update app_Version2.js
    Signed-off-by: PQP CORE <historynetexpress@gmail.com>

--- app_Version2.js
+++ app_Version2.js
@@ -1,8 +1,1 @@
-const MODELS = [
-  { id: 'openai', name: 'OpenAI', desc: 'The OpenAI model' },
-  { id: 'gpt-3', name: 'GPT-3', desc: 'Generative Pre-trained Transformer 3' },
-  // Other models...
-  // Removed Kimi agent
-];
 
-export default MODELS;
